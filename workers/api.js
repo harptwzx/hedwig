92a2f4605c06d8bd4b2973d3feb49da096de22dc
harptwzx <wzx123_snake@qq.com>
2026-06-10 13:54:00 +0000

    更新 api.js

--- workers/api.js
+++ workers/api.js
@@ -1,6 +1,6 @@
 export default {
     async fetch(request, env, ctx) {
-        return new Response('Manual deploy test - ' + new Date().toISOString(), {
+        return new Response('Hello! The website will be finished a few days later...Zhouxiang', {
             headers: { 'Content-Type': 'text/plain' }
         });
     }
