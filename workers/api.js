8414cd0bf2bdd4f6ea2fa93329641ae8e91a634f
harptwzx <wzx123_snake@qq.com>
2026-06-11 22:22:24 +0800

    修改文件

--- workers/api.js
+++ workers/api.js
@@ -1,6 +1,6 @@
 export default {
     async fetch(request, env, ctx) {
-        return new Response('Hello! The website will be finished a few days later...Zhouxiang.', {
+        return new Response('Hello! The website will be finished a few days later...Zhouxiang', {
             headers: { 'Content-Type': 'text/plain' }
         });
     }
