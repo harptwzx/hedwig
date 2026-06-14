bb82f39a53b7767068971a3880ec77fd796363e7
harptwzx <wzx123_snake@qq.com>
2026-06-14 19:49:43 +0800

    删除项目

--- functions/auth/login.js
+++
@@ -1,6 +0,0 @@
-export function onRequest(context) {
-    const clientId = context.env.GITHUB_CLIENT_ID;
-    const redirectUri = 'https://hedwig-e85.pages.dev/auth/callback';
-    const githubUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=read:user`;
-    return Response.redirect(githubUrl, 302);
-}
