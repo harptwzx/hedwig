4ed1a4ab1ca400a309bd7d604cc43d17c0437705
harptwzx <wzx123_snake@qq.com>
2026-07-24 13:57:17 +0000

    创建 index.js

---
+++ workers/index.js
@@ -0,0 +1,16 @@
+// workers/index.js
+export default {
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+        
+        // Hugging Face 代理路由
+        if (url.pathname.startsWith('/hf')) {
+            const hfProxy = await import('./hf-proxy.js');
+            return hfProxy.default.fetch(request, env, ctx);
+        }
+        
+        // 原有网站路由
+        const api = await import('./api.js');
+        return api.default.fetch(request, env, ctx);
+    },
+};
