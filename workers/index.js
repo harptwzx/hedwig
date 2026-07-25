5c3872725043888951b688b1eceef87ac8dbb47c
harptwzx <wzx123_snake@qq.com>
2026-07-25 04:23:48 +0000

    更新 index.js

--- workers/index.js
+++ workers/index.js
@@ -2,13 +2,7 @@
 export default {
     async fetch(request, env, ctx) {
         const url = new URL(request.url);
-        
-        // Hugging Face 代理路由
-        if (url.pathname.startsWith('/hf')) {
-            const hfProxy = await import('./share.js');
-            return hfProxy.default.fetch(request, env, ctx);
-        }
-        
+             
         // 原有网站路由
         const api = await import('./api.js');
         return api.default.fetch(request, env, ctx);
