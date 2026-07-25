0b5044c99189dcf891572bbe582741e71b3aca09
harptwzx <wzx123_snake@qq.com>
2026-07-25 02:24:11 +0000

    更新 index.js

--- workers/index.js
+++ workers/index.js
@@ -9,6 +9,12 @@ export default {
             return hfProxy.default.fetch(request, env, ctx);
         }
         
+        // 文件传输系统路由
+        if (url.pathname.startsWith('/share')) {
+            const hfProxy = await import('./share/file-share.js');
+            return hfProxy.default.fetch(request, env, ctx);
+        }
+
         // 原有网站路由
         const api = await import('./api.js');
         return api.default.fetch(request, env, ctx);
